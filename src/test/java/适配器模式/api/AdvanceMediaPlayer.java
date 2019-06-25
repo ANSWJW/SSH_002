af8ea2a81afcd1b00a952ab31b4a8d9b24d9142c
wjw <wjw_allen@163.com>
2019-06-25 20:57:01 +0800

    适配器模式

---
+++ src/test/java/适配器模式/api/AdvanceMediaPlayer.java
@@ -0,0 +1,9 @@
+package 适配器模式.api;
+
+/**
+ * Created by WJW on 2019/6/25.
+ */
+public interface AdvanceMediaPlayer {
+    public void playVlc(String fileName);
+    public void palyMp4(String fileName);
+}
--- src/test/java/适配器模式/api/MediaPlayer.java
+++ src/test/java/适配器模式/api/MediaPlayer.java
@@ -1,5 +1,8 @@
 package 适配器模式.api;
 
+/**
+ * Created by WJW on 2019/6/25.
+ */
 public interface MediaPlayer {
-	public void play(String audioType, String fileName);
+    public void play(String audioType,String fileName);
 }
