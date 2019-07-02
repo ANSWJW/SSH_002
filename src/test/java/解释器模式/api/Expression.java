4f7b7f570c2d0b1a7da4b7bfdb1f9786ef4a3d19
wjw <wjw_allen@163.com>
2019-07-02 23:51:27 +0800

    解释器模式

---
+++ src/test/java/解释器模式/api/Expression.java
@@ -0,0 +1,8 @@
+package 解释器模式.api;
+
+/**
+ * Created by WJW on 2019/7/2.
+ */
+public interface Expression {
+    public boolean interpret(String context);
+}
---
+++ src/test/java/解释器模式/imp/OrExpression.java
@@ -0,0 +1,21 @@
+package 解释器模式.imp;
+
+import 解释器模式.api.Expression;
+
+/**
+ * Created by WJW on 2019/7/2.
+ */
+public class OrExpression implements Expression {
+    private Expression expr1 = null;
+    private Expression expr2 = null;
+
+    public OrExpression(Expression expr1, Expression expr2) {
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        return expr1.interpret(context) || expr2.interpret(context);
+    }
+}
