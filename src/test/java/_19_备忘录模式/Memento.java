e5884809ec84b9f38ce88ee2e040670892834c03
wjw <wjw_allen@163.com>
2019-07-03 21:47:05 +0800

    备忘录模式

---
+++ src/test/java/_19_备忘录模式/Memento.java
@@ -0,0 +1,13 @@
+package _19_备忘录模式;
+
+public class Memento {
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public Memento(String state) {
+        this.state = state;
+    }
+}
