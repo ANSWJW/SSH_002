3b855d705046ef628711b6d938bca6ba3f2f4cf6
wjw <wjw_allen@163.com>
2019-07-04 22:54:36 +0800

    空对象模式

--- src/test/java/_22_空对象模式/api/AbstractCustomer.java
+++ src/test/java/_22_空对象模式/api/AbstractCustomer.java
@@ -1,7 +1,11 @@
 package _22_空对象模式.api;
 
+/**
+ *
+ */
 public abstract class AbstractCustomer {
     protected String name;
     public abstract boolean isNil();
     public abstract String getName();
+
 }
